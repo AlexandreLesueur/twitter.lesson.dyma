b705941d467fdf8ca9f7bbdd33a9ff748707657e
Alexandre LESUEUR <alesueur@cosem.fr>
2019-08-13 23:43:45 +0200

    cert

--- environment/production.js
+++ environment/production.js
@@ -1,7 +1,7 @@
 module.exports = {
   dbUrl: 'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true',
-  cert: '/etc/letsencrypt/live/www.dyma-projects.site/fullchain.pem',
-  key: '/etc/letsencrypt/live/www.dyma-projects.site/privkey.pem',
+  cert: '/etc/letsencrypt/live/www.formation-node.lesua.fr/fullchain.pem',
+  key: '/etc/letsencrypt/live/www.formation-node.lesua.fr/privkey.pem',
   portHttp: 80,
   portHttps: 443
 }
