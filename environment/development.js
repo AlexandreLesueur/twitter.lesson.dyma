239d84e51a5d49fa79cf9ef9769c70cf86daa178
Alexandre LESUEUR <alesueur@cosem.fr>
2019-08-13 23:24:58 +0200

    prod

--- environment/development.js
+++ environment/development.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  dbUrl: 'mongodb+srv://alex:alex@cluster0-hai9j.mongodb.net/twitter?retryWrites=true&w=majority',
+  dbUrl: 'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true',
   cert: path.join( __dirname, '../ssl/local.crt'),
   key: path.join( __dirname, '../ssl/local.key'),
+  portHttp: 3000,
+  portHttps: 3001
 }
--- environment/production.js
+++ environment/production.js
@@ -1,5 +1,7 @@
 module.exports = {
-    dbUrl: 'mongodb+srv://alex:alex@cluster0-hai9j.mongodb.net/twitter?retryWrites=true&w=majority',
-    cert: '',
-    key: ''
-  }
+  dbUrl: 'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true',
+  cert: '/etc/letsencrypt/live/www.dyma-projects.site/fullchain.pem',
+  key: '/etc/letsencrypt/live/www.dyma-projects.site/privkey.pem',
+  portHttp: 80,
+  portHttps: 443
+}
